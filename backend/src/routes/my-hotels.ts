d5e3bc3374a2d2971e594215ecde8b6f4f35d589
kenneth <kennytakudzwa@gmail.com>
2024-07-14 16:46:06 +0200

    functionality to view my hotels

--- backend/src/routes/my-hotels.ts
+++ backend/src/routes/my-hotels.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response } from 'express'
 import multer from 'multer'
 import cloudinary from 'cloudinary'
-import Hotel, { HotelType } from '../models/hotels'
+import Hotel, { HotelType } from '../models/hotel'
 import verifyToken from '../middleware/auth'
 import { body } from 'express-validator'
 
@@ -69,5 +69,14 @@ router.post(
     }
   }
 )
+
+router.get('/', verifyToken, async (req: Request, res: Response) => {
+  try {
+    const hotels = await Hotel.find({ userId: req.userId })
+    res.json(hotels)
+  } catch (error) {
+    res.status(500).json({ message: 'Error fetching hotels' })
+  }
+})
 
 export default router
